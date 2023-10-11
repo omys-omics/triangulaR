04f01fa3e26292085d369eb1f4cf328b1a38ef3a
Ben Wiens <bwiens36@gmail.com>
2023-10-11 13:56:25 -0500

    Added example data

---
+++ R/data_documentation.R
@@ -0,0 +1,18 @@
+#' Example of a vcfR object
+#'
+#' An S4 object of class vcfR
+#'
+#' @examples
+#' data("example.vcfR", package = "triangulaR")
+#'
+"example.vcfR"
+
+
+#' Example of a popmap
+#'
+#' An S3 object of class data.frame
+#'
+#' @examples
+#' data("example.popmap", package = "triangulaR")
+#'
+"example.popmap"
