dc49c4b36997167f560fd0cb2cbe526b07270395
Ben Wiens <bwiens36@gmail.com>
2023-10-12 08:58:29 -0500

    startup message

--- R/zzz.R
+++ R/zzz.R
@@ -3,10 +3,10 @@
   pkg.version <- utils::packageVersion("triangulaR")
   packageStartupMessage(
     c(paste0("This is triangulaR v.", pkg.version, "\n\n"),
-      "   /\   ",
-      "  /  \  ",
-      " /    \ ",
-      "/______\ ",
+      "   /\
+         /  \
+        /    \
+       /______\ ",
       "Usage information is available at: https://github.com/omys-omics/triangulaR/ \n\n",
       "Please cite the following if you use triangulaR in a publication: \n\n",
       "")
