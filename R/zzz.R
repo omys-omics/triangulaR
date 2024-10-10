c242e8cbb9a596cacd038905cfec79d35e193321
Ben Wiens <bwiens36@gmail.com>
2024-10-10 08:34:13 -0500

    Update citation

--- R/zzz.R
+++ R/zzz.R
@@ -9,6 +9,6 @@
        /______\\", "\n\n",
       "Usage information is available at: https://github.com/omys-omics/triangulaR/ \n\n",
       "Please cite the following if you use triangulaR in a publication: \n\n",
-      "")
+      "Wiens, B. J., & Colella, J. P. (2024). triangulaR: an R package for identifying AIMs and building triangle plots using SNP data from hybrid zones. bioRxiv, 2024-03.")
   )
 }
